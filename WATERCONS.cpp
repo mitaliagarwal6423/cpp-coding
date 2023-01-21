ee792676eb579b053a553716aacbf3434e25f630
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-01-21 15:12:24 +0530

    Create WATERCONS.cpp

---
+++ WATERCONS.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>x;
+        if(x<2000)
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
