9a88e4d7ec41c16eccd2fd41888e68510f115505
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-01-21 18:21:17 +0530

    Create BURGERS.cpp

---
+++ BURGERS.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,a,b;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>a>>b;
+        if(a<b)
+        {
+            cout<<a<<endl;
+        }
+        else
+        {
+        cout<<b<<endl;
+        }
+    
+    }
+	// your code goes here
+	return 0;
+}
