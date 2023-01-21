61b74ed59c2626b6aa2edc8ba11001ac08340dae
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-01-21 15:08:35 +0530

    Create SQUATS.cpp

---
+++ SQUATS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>x;
+        int y = x*15;
+        cout<<y<<endl;
+    }
+    
+	// your code goes here
+	return 0;
+}
