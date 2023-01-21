348ae620fc845c3ae89605493a03fe9969af1efc
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-01-21 18:29:42 +0530

    Create DETSCORE.cpp

---
+++ DETSCORE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>n;
+        int y = x/10;
+        int z= n*y;
+        cout<<z<<endl;
+    }
+	// your code goes here
+	return 0;
+}
