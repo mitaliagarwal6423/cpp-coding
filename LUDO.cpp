37c09d5ed74e37522ea0b092d6efb2f40d9d2cfa
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-01-21 18:11:54 +0530

    Create LUDO.cpp

---
+++ LUDO.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+    cin>>x;
+    if(x==6)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+    }
+	// your code goes here
+	return 0;
+}
