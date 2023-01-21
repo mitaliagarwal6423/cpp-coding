59cb5e0c6f89f5e8daf999bcf6c475e44b8407d3
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-01-21 15:44:03 +0530

    code

---
+++ BIRYANI.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,y;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>x>>y;
+        int z= x*y;
+        cout<<z<<endl;
+    }
+	// your code goes here
+	return 0;
+}
---
+++ FIT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>x;
+        int y = x *2*5;
+        cout<<y<<endl;
+    }
+	// your code goes here
+	return 0;
+}
