178588df1335eca61a6439eba8cf1a479353a9fa
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-01-28 15:50:37 +0530

    Create TALLER.cpp

---
+++ TALLER.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,y;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y;
+        if(x>y)
+        {
+            cout<<"A"<<endl;
+        }
+        else
+        {
+            cout<<"B"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
