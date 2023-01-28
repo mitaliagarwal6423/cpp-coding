114d5c59eb0b24c0ad2faf474c859cc80cb122fa
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-01-28 16:04:18 +0530

    Create REACHTARGET.cpp

---
+++ REACHTARGET.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,y;
+    cin>>t;
+    while(t--)
+    {
+        cin>>y>>x;
+        int z=y-x;
+        cout<<z<<endl;
+    }
+	// your code goes here
+	return 0;
+}
