cc0c280e9d5a999de19be812a96c04d95cf235ac
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-01-29 16:46:50 +0530

    Create KNGTOR.cpp

---
+++ KNGTOR.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,n,m;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>m;
+        int total = n*5 +m*7;
+        cout<<total<<endl;
+        
+    }
+	// your code goes here
+	return 0;
+}
