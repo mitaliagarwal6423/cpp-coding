3c49505421922ab07313af566b4f6bc930c23438
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-01-29 18:54:32 +0530

    Create MY1STCONTEST.cpp

---
+++ MY1STCONTEST.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,a,b;
+    cin>>n>>a>>b;
+    int t = n-a;
+    int c = t-b;
+    cout<<t<<" "<<c;
+	// your code goes here
+	return 0;
+}
