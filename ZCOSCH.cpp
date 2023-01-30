360012b42da1621cd0aef691cba737fcc8c9e70d
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-01-30 18:29:00 +0530

    Create ZCOSCH.cpp

---
+++ ZCOSCH.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r;
+    cin>>r;
+    if(r>=1 && r<=50)
+    {
+        cout<<"100"<<endl;
+    }
+    else if(r>=51 && r<=100)
+    {
+        cout<<"50"<<endl;
+    }
+    else
+    {
+        cout<<"0"<<endl;
+    }
+	// your code goes here
+	return 0;
+}
