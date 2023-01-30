a8147e41cf57ef672d43ac8d792de9059d868ffa
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-01-30 18:25:45 +0530

    Create MINHEIGHT.cpp

---
+++ MINHEIGHT.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,h;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>h;
+        if(x>=h)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
