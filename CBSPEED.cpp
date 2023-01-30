62a52d995c573f8bb8db01eef4e252f0217b59cd
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-01-30 18:32:37 +0530

    Create CBSPEED.cpp

---
+++ CBSPEED.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+    if(x>=y)
+    {
+        cout<<"NO"<<endl;
+    }
+    else
+    {
+        cout<<"YES"<<endl;
+    }
+	// your code goes here
+	return 0;
+}
