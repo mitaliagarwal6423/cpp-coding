23446c10b0a37c312a7f3fd9db9b3e2c38dbe431
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-01 17:30:48 +0530

    Create INSTAGRAM.cpp

---
+++ INSTAGRAM.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,y;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y;
+        int z = 10*y;
+        if(x>z)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
