cb8cdea7b943f97c0af2c569915b171c20da1cd5
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-04 17:22:34 +0530

    Create HOTCOLD.cpp

---
+++ HOTCOLD.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,c;
+    cin>>t;
+    while(t--)
+    {
+        cin>>c;
+        if(c>20)
+        {
+            cout<<"HOT"<<endl;
+        }
+        else
+        {
+            cout<<"COLD"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
