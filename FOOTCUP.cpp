070b1a18a4c0811a8d1acf26dccd8d9646a95f73
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-04 17:45:15 +0530

    Create FOOTCUP.cpp

---
+++ FOOTCUP.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,y;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y;
+        if(x==y && x!=0 && y!=0)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
