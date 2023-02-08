f5d8fa594a555f9951658bd16a00b744445f7899
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-08 12:22:42 +0530

    Create CNTWRD.cpp

---
+++ CNTWRD.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,n,m;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>m;
+        int total = n *m;
+        cout<<total<<endl;
+    }
+	// your code goes here
+	return 0;
+}
