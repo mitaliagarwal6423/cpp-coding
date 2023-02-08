9d5ee226c0e1cfabda8ee75a83d07be499719dbd
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-08 12:21:29 +0530

    Create PRIZEPOOL.cpp

---
+++ PRIZEPOOL.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,y;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y;
+        int total = (10 * x) + (90 * y);
+        cout<<total<<endl;
+    }
+	// your code goes here
+	return 0;
+}
