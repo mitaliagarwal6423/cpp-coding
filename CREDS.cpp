f91b1940fa93b37e8f0290bc8d026ab82c0a4bf1
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-10 12:47:45 +0530

    Create CREDS.cpp

---
+++ CREDS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,y,z;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y>>z;
+        int result = (4 * x) + (2 * y) + (0 * z);
+        cout<<result<<endl;
+    }
+    
+	// your code goes here
+	return 0;
+}
