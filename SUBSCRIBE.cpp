8572b3fff86f8a20bff8aeb7f354f219531c82c1
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-11 12:26:48 +0530

    Create SUBSCRIBE.cpp

---
+++ SUBSCRIBE.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x;
+        if(x>30)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+    
+	// your code goes here
+	return 0;
+}
