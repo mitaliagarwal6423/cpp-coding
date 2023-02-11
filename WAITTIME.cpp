c607d550408124cac592d972f295b8485048ce8a
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-11 12:32:33 +0530

    Create WAITTIME.cpp

---
+++ WAITTIME.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,k;
+    cin>>t;
+    while(t--)
+    {
+        cin>>k>>x;
+        int result = (7 *k)-x;
+        cout<<result<<endl;
+    }
+	// your code goes here
+	return 0;
+}
