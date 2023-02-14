f3fe34d4d4371791e0816e8d66aa513aa4c60f70
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-14 11:29:40 +0530

    Create PODIUM.cpp

---
+++ PODIUM.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,a,b;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        int result = a+b;
+        cout<<result<<endl;
+    }
+	// your code goes here
+	return 0;
+}
