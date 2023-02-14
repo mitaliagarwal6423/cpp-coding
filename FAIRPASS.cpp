dc16bb65b96b1dfea478bb55e4c16bf70d283129
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-14 12:49:31 +0530

    Create FAIRPASS.cpp

---
+++ FAIRPASS.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int t,n,k;
+     cin>>t;
+     while(t--)
+     {
+         cin>>n>>k;
+         if(n<k)
+         {
+             cout<<"YES"<<endl;
+         }
+         else
+         {
+             cout<<"NO"<<endl;
+         }
+     }
+    
+	// your code goes here
+	return 0;
+}
