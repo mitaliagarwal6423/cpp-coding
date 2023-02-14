e4477e0a6086127d88a303b15fdf5d12f1942e65
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-14 11:24:24 +0530

    Create MAXDIFFMIN.cpp

---
+++ MAXDIFFMIN.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,a,b,c;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b>>c;
+        int result =  c-a;
+        cout<<result<<endl;
+    }
+	// your code goes here
+	return 0;
+}
