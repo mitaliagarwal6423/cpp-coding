6e28c65f4ed6ae95fe7bba9e0e52f522114bdbe4
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-15 17:21:28 +0530

    Create SLEEP.cpp

---
+++ SLEEP.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x;
+        if(x<7)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
