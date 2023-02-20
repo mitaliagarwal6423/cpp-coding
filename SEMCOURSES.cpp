a40d162e91388c5637333d0fd0169c91e0af0c4a
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-20 17:57:38 +0530

    Create SEMCOURSES.cpp

---
+++ SEMCOURSES.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,y,z;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y>>z;
+        int result = x*y*z;
+        cout<<result<<endl;
+    }
+
+	// your code goes here
+	return 0;
+}
