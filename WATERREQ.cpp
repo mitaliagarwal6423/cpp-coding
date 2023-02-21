93298264b086b8f5d04099e6d1e4d4259ee94257
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-02-21 22:30:16 +0530

    Create WATERREQ.cpp

---
+++ WATERREQ.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        int result = n*2;
+        cout<<result<<endl;
+    }
+	// your code goes here
+	return 0;
+}
