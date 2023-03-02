b1bf1adbb573efacafffc5d4d6cd23a134c155ed
mitaliagarwal6423 <mitaliagarwal6423@gmail.com>
2023-03-02 14:42:37 +0530

    Create APPLORNG.cpp

---
+++ APPLORNG.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,a,b;
+    cin>>x;
+    cin>>a>>b;
+    int ans = a+b;
+    if(ans<=x)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+    
+	// your code goes here
+	return 0;
+}
